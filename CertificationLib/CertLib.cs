c6ed17f0d5699071c518d4be51a1bfbed49bd3f2
JOOOJ <jjjrachel@hotmail.com>
2015-03-24 11:38:37 +0800

    Add Encrypt and Decrypt function

--- CertificationLib/CertLib.cs
+++ CertificationLib/CertLib.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
@@ -18,9 +15,9 @@ namespace CertificationLib
         /// <param name="certSubject">Certification subject name</param>
         /// <param name="content">The content you want to sign</param>
         /// <returns></returns>
-        public string Sign(StoreName storeName,StoreLocation storeLocation,string certSubject,string content)
+        public string Sign(StoreName storeName, StoreLocation storeLocation, string certSubject, string content)
         {
-            if(string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(content))
+            if (string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(content))
             {
                 throw new ArgumentException();
             }
@@ -28,7 +25,7 @@ namespace CertificationLib
             X509Certificate2Collection cols = store.Certificates;
             foreach (X509Certificate2 item in cols)
             {
-                if(item.Subject==certSubject)
+                if (item.Subject == certSubject)
                 {
                     RSACryptoServiceProvider privateKeyProvider = new RSACryptoServiceProvider();
                     privateKeyProvider.FromXmlString(item.PrivateKey.ToXmlString(true));
@@ -47,9 +44,9 @@ namespace CertificationLib
         /// <param name="content">The content you want to sign</param>
         /// <param name="signContent">The sign result</param>
         /// <returns></returns>
-        public bool Verify(StoreName storeName, StoreLocation storeLocation, string certSubject, string content,string signContent)
+        public bool Verify(StoreName storeName, StoreLocation storeLocation, string certSubject, string content, string signContent)
         {
-            if(string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(content) || string.IsNullOrEmpty(signContent))
+            if (string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(content) || string.IsNullOrEmpty(signContent))
             {
                 throw new ArgumentException();
             }
@@ -57,7 +54,7 @@ namespace CertificationLib
             X509Certificate2Collection cols = store.Certificates;
             foreach (X509Certificate2 item in cols)
             {
-                if(item.Subject==certSubject)
+                if (item.Subject == certSubject)
                 {
                     RSACryptoServiceProvider publicKeyProvider = new RSACryptoServiceProvider();
                     publicKeyProvider.FromXmlString(item.PublicKey.Key.ToXmlString(false));
@@ -66,17 +63,78 @@ namespace CertificationLib
             }
             return false;
         }
-        private string Sign(RSACryptoServiceProvider provider,string content)
+
+        /// <summary>
+        /// Encrypt content
+        /// </summary>
+        /// <param name="storeName">System.Security.Cryptography.X509Certificates.StoreName</param>
+        /// <param name="storeLocation">System.Security.Cryptography.X509Certificates.StoreLocation</param>
+        /// <param name="certSubject">Certification subject name</param>
+        /// <param name="content">The content you want to sign</param>
+        /// <returns>Encrypt content result</returns>
+        public string RSAEncrypt(StoreName storeName, StoreLocation storeLocation, string certSubject, string content)
+        {
+            if (string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentException();
+            }
+            X509Store store = new X509Store(storeName, storeLocation);
+            X509Certificate2Collection cols = store.Certificates;
+            foreach (X509Certificate2 item in cols)
+            {
+                if (item.Subject == certSubject)
+                {
+                    RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+                    provider.FromXmlString(item.PrivateKey.ToXmlString(true));
+                    byte[] rgb = new UnicodeEncoding().GetBytes(content);
+                    byte[] result = provider.Encrypt(rgb, false);
+                    return Convert.ToBase64String(result);
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Decrypt content
+        /// </summary>
+        /// <param name="storeName">System.Security.Cryptography.X509Certificates.StoreName</param>
+        /// <param name="storeLocation">System.Security.Cryptography.X509Certificates.StoreLocation</param>
+        /// <param name="certSubject">Certification subject name</param>
+        /// <param name="encryptContent">The encrypt content</param>
+        /// <returns>Decrypt result</returns>
+        public string RSADecrypt(StoreName storeName, StoreLocation storeLocation, string certSubject, string encryptContent)
+        {
+            if (string.IsNullOrEmpty(certSubject) || string.IsNullOrEmpty(encryptContent))
+            {
+                throw new ArgumentException();
+            }
+            X509Store store = new X509Store(storeName, storeLocation);
+            X509Certificate2Collection cols = store.Certificates;
+            foreach (X509Certificate2 item in cols)
+            {
+                if (item.Subject == certSubject)
+                {
+                    RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+                    provider.FromXmlString(item.PublicKey.Key.ToXmlString(false));
+                    byte[] rgb = new UnicodeEncoding().GetBytes(encryptContent);
+                    byte[] result = provider.Decrypt(rgb, true);
+                    return Convert.ToBase64String(result);
+                }
+            }
+            return null;
+        }
+
+        private string Sign(RSACryptoServiceProvider provider, string content)
         {
             UnicodeEncoding encoding = new UnicodeEncoding();
             byte[] data = encoding.GetBytes(content);
             SHA1Managed sha1 = new SHA1Managed();
             byte[] rgbHash = sha1.ComputeHash(data);
-            byte [] result = provider.SignHash(rgbHash, CryptoConfig.MapNameToOID("SHA1"));
+            byte[] result = provider.SignHash(rgbHash, CryptoConfig.MapNameToOID("SHA1"));
             return Convert.ToBase64String(result);
         }
 
-        private bool Verify(RSACryptoServiceProvider provider,string content,string signContent)
+        private bool Verify(RSACryptoServiceProvider provider, string content, string signContent)
         {
             UnicodeEncoding encoding = new UnicodeEncoding();
             byte[] data = encoding.GetBytes(content);
